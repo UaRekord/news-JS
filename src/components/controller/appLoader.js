daad8d7ac8572ef791b146aecb73f58cd1b27d3b
warlock150@gmail.com <email@example.com>
2023-06-12 21:08:15 +0200

    added TS to project

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'eebf7467d4ae4760ad6a36e5689b8e68', // получите свой ключ https://newsapi.org/
         });
     }
 }
